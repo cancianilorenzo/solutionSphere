5f157bf4ad3f157d7560d48b0c2a52fb5a8dfc3b
Lorenzo <cancianilorenzo@gmail.com>
2024-06-16 16:52:14 +0200

    Fix: merge conflicts

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const port = 3001;
 
 //Cors options
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: "http://localhost:5173",
   credentials: true,
 };
 app.use(cors(corsOptions));
@@ -45,7 +45,6 @@ passport.deserializeUser(function (user, callback) {
   return callback(null, user);
 });
 
-//Session creation
 const session = require("express-session");
 
 app.use(
@@ -77,6 +76,8 @@ const validCategories = [
   "payment",
 ];
 
+const validStates = ["open", "closed"];
+
 app.get("/api/tickets", (req, res) => {
   ticketDao
     .listTickets()
@@ -84,39 +85,16 @@ app.get("/api/tickets", (req, res) => {
     .catch((err) => res.status(500).json(err));
 });
 
-app.get(
-  "/api/blocks",
-  [
-    query("id")
-      .not()
-      .isEmpty()
-      .withMessage("Id should not be empty")
-      .isInt()
-      .withMessage("Id should be an integer"),
-    isLoggedIn,
-  ],
-  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    const { id } = req.query;
-    ticketDao
-      .listBlocksByTicket(id)
-      .then((blocks) => res.json(blocks))
-      .catch((err) => res.status(500).json(err));
-  }
-);
+app.get("/api/blocks", [isLoggedIn], (req, res) => {
+  ticketDao
+    .listBlocks()
+    .then((blocks) => res.json(blocks))
+    .catch((err) => res.status(500).json(err));
+});
 
 app.post(
   "/api/tickets",
   [
-    body("owner")
-      .not()
-      .isEmpty()
-      .withMessage("Owner should not be empty")
-      .isInt()
-      .withMessage("Owner should be an integer"),
     body("title")
       .not()
       .isEmpty()
@@ -143,8 +121,7 @@ app.post(
     }
     const ticket = req.body;
     ticket.owner = req.user.id;
-    //Check user existance on db
-    if(!userDao.getUserById(ticket.owner)){
+    if (!userDao.getUserById(ticket.owner)) {
       return res.status(400).json({ error: "User does not exist" });
     }
     ticketDao
@@ -157,12 +134,6 @@ app.post(
 app.post(
   "/api/blocks",
   [
-    body("author")
-      .not()
-      .isEmpty()
-      .withMessage("Author should not be empty")
-      .isInt()
-      .withMessage("Author should be an integer"),
     body("text")
       .not()
       .isEmpty()
@@ -184,12 +155,10 @@ app.post(
     }
     const block = req.body;
     block.author = req.user.id;
-    //Check user existance on db
-    if(!userDao.getUserById(block.author)){
+    if (!userDao.getUserById(block.author)) {
       return res.status(400).json({ error: "User does not exist" });
     }
-    //check ticket existance on db
-    if(!ticketDao.getTicketById(block.ticketId)){
+    if (!ticketDao.getTicketById(block.ticketId)) {
       return res.status(400).json({ error: "Ticket does not exist" });
     }
     ticketDao
@@ -199,8 +168,8 @@ app.post(
   }
 );
 
-app.put(
-  "/api/categoryTicket",
+app.patch(
+  "/api/ticket",
   [
     body("id")
       .not()
@@ -209,91 +178,41 @@ app.put(
       .isInt()
       .withMessage("Id should be an integer"),
     body("category")
+      .optional()
       .isIn(validCategories)
       .withMessage(
         `Category must be one of the following: ${validCategories.join(", ")}`
       ),
+    body("state")
+      .optional()
+      .isIn(validStates)
+      .withMessage(
+        `State must be one of the following: ${validStates.join(", ")}`
+      ),
     isLoggedIn,
   ],
-  (req, res) => {
-    //Better to use patch??
-    if (req.user.role !== "admin") {
-      return res.status(403).json({ error: "Not authorized" });
-    }
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    const ticket = req.body;
-    //check ticket existance on db
-    if(!ticketDao.getTicketById(ticket.id)){
+    const oldTicket = await ticketDao.getTicketById(req.body.id);
+    if (!oldTicket) {
       return res.status(400).json({ error: "Ticket does not exist" });
     }
+    if (req.user.role !== "admin" && (req.body.category!== oldTicket.category)) {
+      return res.status(403).json({ error: "Not authorized to change category" });
+    }
+    if (req.user.role !== "admin" && req.body.state === "open") {
+      return res.status(403).json({ error: "Not authorized to put a open" });
+    }
+    if (oldTicket.owner !== req.user.id && req.user.role !== "admin") {
+      return res.status(403).json({ error: "Not authorized, id different" });
+    }
+    oldTicket.category = req.body.category || oldTicket.category;
+    oldTicket.state = req.body.state || oldTicket.state;
     ticketDao
-      .patchTicketCategory(ticket)
-      .then((response) => res.json(response))
-      .catch((err) => res.status(500).json(err));
-  }
-);
-
-app.put(
-  "/api/openTicket",
-  [
-    body("id")
-      .not()
-      .isEmpty()
-      .withMessage("Id should not be empty")
-      .isInt()
-      .withMessage("Id should be an integer"),
-    isLoggedIn,
-  ],
-  (req, res) => {
-    if (req.user.role !== "admin") {
-      return res.status(403).json({ error: "Not authorized" });
-    }
-    //Better to use patch??
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    const ticket = req.body;
-    //check ticket existance on db
-    if(!ticketDao.getTicketById(ticket.id)){
-      return res.status(400).json({ error: "Ticket does not exist" });
-    }
-    ticketDao
-      .setTicketOpened(ticket)
-      .then((response) => res.json(response))
-      .catch((err) => res.status(500).json(err));
-  }
-);
-
-app.put(
-  "/api/closeTicket",
-  [
-    body("id")
-      .not()
-      .isEmpty()
-      .withMessage("Id should not be empty")
-      .isInt()
-      .withMessage("Id should be an integer"),
-      isLoggedIn,
-  ],
-  (req, res) => {
-    if(ticket.owner !== req.user.id){
-      return res.status(403).json({ error: "Not authorized" });
-    }
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    const ticket = req.body;
-    //check ticket existance on db
-    if(!ticketDao.getTicketById(ticket.id)){
-      return res.status(400).json({ error: "Ticket does not exist" });
-    }
-    ticketDao
-      .setTicketClosed(ticket)
+      .patchTicket(oldTicket)
       .then((response) => res.json(response))
       .catch((err) => res.status(500).json(err));
   }
@@ -303,15 +222,10 @@ app.post("/api/sessions", function (req, res, next) {
   passport.authenticate("local", (err, user, info) => {
     if (err) return next(err);
     if (!user) {
-      // display wrong login messages
       return res.status(401).json({ error: info });
     }
-    // success, perform the login and extablish a login session
     req.login(user, (err) => {
       if (err) return next(err);
-
-      // req.user contains the authenticated user, we send all the user info back
-      // this is coming from userDao.getUser() in LocalStratecy Verify Fn
       return res.json(req.user);
     });
   })(req, res, next);
